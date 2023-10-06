600539848ab5d23ff877df02342012422be67859
zzzzDenzzzz <12den24@gmail.com>
2023-10-06 12:06:42 +0300

    Добавьте файлы проекта.

---
+++ MovieApp/Services/IMovieApiService.cs
@@ -0,0 +1,10 @@
+﻿using MovieApp.Models;
+
+namespace MovieApp.Services
+{
+    public interface IMovieApiService
+    {
+        Task<MovieApiResponse> SearchByTitleAsync(string title);
+        Task<Models.Cinema> SearchByIdAsync(string id);
+    }
+}
